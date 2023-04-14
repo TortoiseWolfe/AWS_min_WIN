59085d564d476590899ada1d19c64856b250e8a8
Turtle Wolfe <JonPohlner@gmail.com>
2023-04-14 13:38:27 -0400

    use the pre-defined values of the enumeration

--- shutdown_prompt.ps1
+++ shutdown_prompt.ps1
@@ -1,10 +1,16 @@
 Add-Type -AssemblyName PresentationFramework
 
 $timeoutSeconds = 300
-$confirmation = New-Object -TypeName System.Windows.MessageBoxButton -ArgumentList @( 'Yes', 'No', 'Timeout' )
-$modalResult = [System.Windows.MessageBox]::Show('Do you want to terminate the instance?', 'Termination Confirmation', $confirmation, [System.Windows.MessageBoxImage]::Question, [System.Windows.MessageBoxResult]::Timeout, ($timeoutSeconds * 1000))
+$messageBoxText = 'Do you want to terminate the instance?'
+$messageBoxTitle = 'Termination Confirmation'
+$messageBoxButtons = [System.Windows.MessageBoxButton]::YesNo
+$messageBoxImage = [System.Windows.MessageBoxImage]::Question
+$messageBoxDefaultButton = [System.Windows.MessageBoxResult]::Yes
+$messageBoxTimeout = $timeoutSeconds * 1000
 
-if ($modalResult -eq 'Yes' -or $modalResult -eq 'Timeout') {
+$modalResult = [System.Windows.MessageBox]::Show($messageBoxText, $messageBoxTitle, $messageBoxButtons, $messageBoxImage, $messageBoxDefaultButton, $messageBoxTimeout)
+
+if ($modalResult -eq 'Yes') {
     $instance_id = (Invoke-WebRequest -Uri 'http://169.254.169.254/latest/meta-data/instance-id').Content
     aws ec2 terminate-instances --instance-ids $instance_id
 }
