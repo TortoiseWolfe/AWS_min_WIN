2f285e9084bb4366357fa9129f2a7bc99a24fc10
Turtle Wolfe <JonPohlner@gmail.com>
2023-04-18 14:42:27 -0400

    documentation suggest MSI installer

--- userData Scripts/User Data As Text 011.ps1
+++ userData Scripts/User Data As Text 011.ps1
@@ -1,6 +1,6 @@
 <powershell>
 # Set the path for the log file
-$logFilePath = "C:\Program Files\script_log.txt"
+$logFilePath = "C:\Users\Administrator\script_log.txt"
 
 # Check if the user has permission to write to the log file
 try {
@@ -17,23 +17,15 @@ $psVersion = $PSVersionTable.PSVersion
 $psVersionMessage = "PowerShell version: $($psVersion.Major).$($psVersion.Minor)"
 Add-Content -Path $logFilePath -Value $psVersionMessage
 
-# Try to download and install PowerShell 7
+# Try to download and install PowerShell 7 using the MSI package
 try {
-    # Save the current execution policy
-    $currentPolicy = Get-ExecutionPolicy
-    # Set the execution policy to unrestricted
-    Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process -Force
-
-    # Download and install PowerShell 7 from Microsoft repository
-    $installScriptUrl = "https://aka.ms/install-powershell.ps1"
-    Invoke-Expression "& { $(Invoke-WebRequest -UseBasicParsing -Uri $installScriptUrl) }"
-
-    # Restore the execution policy
-    Set-ExecutionPolicy -ExecutionPolicy $currentPolicy -Scope Process -Force
+    $installerUrl = "https://github.com/PowerShell/PowerShell/releases/download/v7.2.1/PowerShell-7.2.1-win-x64.msi"
+    $installerPath = "C:\PowerShellInstaller.msi"
+    (New-Object System.Net.WebClient).DownloadFile($installerUrl, $installerPath)
+    Start-Process -FilePath $installerPath -ArgumentList "/qn" -Wait -NoNewWindow
 } catch {
     Add-Content -Path $logFilePath -Value "Error installing PowerShell 7: $_"
 }
-
 
 # Check if PowerShell 7 was installed successfully
 $pwshPath = "$($env:ProgramFiles)\PowerShell\7\pwsh.exe"
